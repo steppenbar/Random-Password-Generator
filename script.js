fc958c962a78e5e2af3ecdf4e56584651e8ea19c
Steppenbar <hiwattslo@gmail.com>
2020-01-18 00:28:42 -0800

    final

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+/*
+
+this will get us our lower case characters
+function getRamLow(){
+    return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+this will get us our upper case characters
+function getRamUp(){
+    return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+ 
+function getRamNum(){
+    return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+function getRamSymbol(){
+    const symbols = "`!@#$%^&*(){}[]=<>";
+    return symbols[Math.floor(Math.random() * symbols.lenght)];
+}
+*/
 
 
- function generate() { 
+function generate() { 
     //set password length
 //passwrod values
     
-    var complexity = document.getElementById("slider").nodeValue;
+    var complexity = parseInt(document.getElementById("slider").value);
     // password characters
     var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"; 
     var password = "";
@@ -13,6 +34,7 @@
 
     for (var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+        console.log(password)
     }
      //add password to textbox/display area
 
@@ -24,8 +46,9 @@
 
 }
 
-// set default lenght display of 64
-document.getElementById("length").innerHTML = "length: 64";
+// set default lenght display of 68
+
+document.getElementById("length").innerHTML = "length: 68";
 
 // function for lenght  of password based on slider pick
 
